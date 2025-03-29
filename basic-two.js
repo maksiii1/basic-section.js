a7827075e96444048517086df720753dfed166f0
Maksin Spizhuk <indcheat@mail.ru>
2025-03-29 17:53:37 +0300

    added basic-two.js

---
+++ basic-two.js
@@ -0,0 +1,8 @@
+const concat = (firstName, secondName, age) =>
+  `Hello, ${firstName} ${secondName} congratulations  on your ${age}!`;
+
+let fName = "Emily";
+let sName = "Carter";
+let age = 17;
+
+console.log(concat(fName, sName, age));
