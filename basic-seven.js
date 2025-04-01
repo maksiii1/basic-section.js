24cd95dff3ab3d9d0262354693995553a4b70b07
Maksin Spizhuk <indcheat@mail.ru>
2025-04-01 10:32:41 +0300

    added basic-seven.js

---
+++ basic-seven.js
@@ -0,0 +1,15 @@
+const ageProgress = () => {
+  for (let i = 0; i <= 100; i++) {
+    i <= 17
+      ? console.log(`${i} - ребенок`)
+      : i <= 30
+      ? console.log(`${i} - молодой`)
+      : i <= 60
+      ? console.log(`${i} - зрелый`)
+      : i > 60
+      ? console.log(`${i} - старый`)
+      : console.log("Неопределено");
+  }
+};
+
+ageProgress();
